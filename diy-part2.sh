04dc9ecc35519f51e819c97c81e83a4ae7862525
shmily103 <supanshui@gmail.com>
2021-12-17 17:36:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,6 +7,8 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #============================================================
+#add packages
+git clone https://github.com/shmily103/Panzy.git package/Panzy
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
@@ -14,17 +16,17 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # Modify default SSID
 sed -i 's/ssid=OpenWrt/ssid=Panzy/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-#add packages
-git clone https://github.com/shmily103/Panzy.git package/Panzy
 # Modify Automount
 \cp -rf package/Panzy/mount.hotplug package/system/fstools/files
-# fix unblockmusic
-chmod a+x package/Panzy/luci-app-unblockneteasemusic/root/etc/init.d/unblockneteasemusic
+echo 'Add tools'
+mv package/Panzy/upx tools/upx
+mv package/Panzy/ucl tools/ucl
 
+# fix unblockmusic
+# chmod a+x package/Panzy/luci-app-unblockneteasemusic/root/etc/init.d/unblockneteasemusic
 # rm -rf package/feeds/packages/kcptun
 # update Go
-rm -rf feeds/packages/lang/golang
-rm -rf package/feeds/packages/golang
-cp -rf package/Panzy/golang feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang
+# rm -rf package/feeds/packages/golang
+# cp -rf package/Panzy/golang feeds/packages/lang/golang
 # rm -rf package/Panzy/golang
