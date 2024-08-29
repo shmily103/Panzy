7aab5309a358517d9b166cebd268914c7a5bba74
shmily103 <supanshui@gmail.com>
2024-08-30 01:09:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,14 +13,14 @@ git clone https://github.com/shmily103/Panzy.git package/Panzy
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 # Modify WiFi ON
-sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # Modify default SSID
 sed -i 's/ssid=OpenWrt/ssid=Panzy/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # Modify Automount
 \cp -rf package/Panzy/mount.hotplug package/system/fstools/files
 # Add tools
-mv package/Panzy/upx tools/upx
-mv package/Panzy/ucl tools/ucl
+#mv package/Panzy/upx tools/upx
+#mv package/Panzy/ucl tools/ucl
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -32,7 +32,7 @@ sed -i 's/OpenWrt/Panzy/g' package/base-files/files/bin/config_generate
 sed -i 's/eth0/eth6/g' package/base-files/files/etc/board.d/99-default_network
 
 # Fix permission
-chmod a+x -R package/Panzy
+#chmod a+x -R package/Panzy
 
 # Fix aria2
 # sed -i 's/procd_add_jail_mount "$config_file"/procd_add_jail_mount "$ca_certificate" "$certificate" "$rpc_certificate" "$rpc_private_key"/g' feeds/packages/net/aria2/files/aria2.init
